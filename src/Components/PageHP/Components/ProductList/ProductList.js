858f6c85714e1438b5d037838087a96de09b81e9
feyziyev007 <azerfeyziyev@hotmail.com>
2019-12-16 12:50:42 +0400

    adding new section

---
+++ src/Components/PageHP/Components/ProductList/ProductList.js
@@ -0,0 +1,53 @@
+import React from 'react';
+import ProductCard from './ProductCard/ProductCard';
+
+export default class ProductList extends React.Component {
+  render() {
+    const products = [
+      {
+        id: 1,
+        category: 'men',
+        fit: 'skinny',
+        brand: 'buffalo',
+        description: 'a superb pair of jeans',
+        price: 200,
+        color: 'black',
+        size: '28',
+        images: ['img/products/women/skinny/001.png'],
+        date: '2019-12-15'
+      },
+      {
+        id: 2,
+        category: 'women',
+        fit: 'skinny',
+        brand: 'buffalo',
+        description: 'a superb pair of jeans',
+        price: 200,
+        color: 'black',
+        size: '28',
+        images: ['img/products/women/skinny/001.png'],
+        date: '2019-12-15'
+      },
+      {
+        id: 3,
+        category: 'accessory',
+        fit: 'skinny',
+        brand: 'buffalo',
+        description: 'a superb pair of jeans',
+        price: 200,
+        color: 'black',
+        size: '28',
+        images: ['img/products/women/skinny/001.png'],
+        date: '2019-12-15'
+      }
+    ];
+
+    return (
+      <div className="row">
+        {products.map(item => (
+            <ProductCard imgUrl={item.images[0]} imgCategory={item.category} />
+         ))}
+      </div>
+    );
+  }
+}
--- src/Components/PageHP/index.js
+++ src/Components/PageHP/index.js
@@ -1,62 +1,14 @@
 import React from 'react';
 import style from './style.scss';
+import Slider from './Components/Slider/Slider';
+import ProductList from './Components/ProductList/ProductList';
 
 export default class PageHP extends React.Component {
   render() {
     return (
       <section class="page-hp">
-        <div class="">
-          <div
-            id="carouselExampleCaptions"
-            class="carousel slide"
-            data-ride="carousel"
-          >
-            <ol class="carousel-indicators">
-              <li
-                data-target="#carouselExampleCaptions"
-                data-slide-to="0"
-                class="active"
-              ></li>
-              <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
-              <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
-            </ol>
-            <div class="carousel-inner">
-              <div class="carousel-item active">
-                <img
-                  src="../img/PageHP/hp-slider-1.png"
-                  class="d-block w-100"
-                />
-
-                <div class="carousel-caption d-none d-md-block">
-                  <h2>New COllection</h2>
-                  <p>Shop Now</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img
-                  src="../img/PageHP/hp-slider-2.png"
-                  class="d-block w-100"
-                  alt="..."
-                />
-                <div class="carousel-caption d-none d-md-block">
-                  <h2>END-OF-SEASON SALE</h2>
-                  <p>Shop Now</p>
-                </div>
-              </div>
-              <div class="carousel-item">
-                <img
-                  src="../img/PageHP/hp-slider-3.png"
-                  class="d-block w-100"
-                  alt="..."
-                />
-                <div class="carousel-caption d-none d-md-block">
-                  <h2>ENJOY 70% OFF</h2>
-                  <p>Shop Now</p>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
+        <Slider />
+        <ProductList />
       </section>
     );
   }
