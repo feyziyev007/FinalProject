e4f87906e40cc642fe58b41a73068ae7b850d019
imanorujov <iman0orujov@gmail.com>
2019-12-18 15:18:24 +0400

    tried to fetch some data

--- src/Components/PagePLP/Components/Categories/categories.js
+++ src/Components/PagePLP/Components/Categories/categories.js
@@ -3,9 +3,9 @@ import style from './style.scss';
 
 class Categories extends React.Component {
   render(){
-    const {category,size,brand, class_name} = this.props;
+    const {category,fit,brand, class_name} = this.props;
     console.log(this.props);
-    return <label className={class_name}>{category}{size}{brand}</label>;
+    return <label className={class_name}>{category}{fit}{brand}</label>;
   }
 }
 
--- src/Components/PagePLP/pagePLP.js
+++ src/Components/PagePLP/pagePLP.js
@@ -4,31 +4,32 @@ import ListItem from './Components/ListItem/ListItem';
 import Categories from "./Components/Categories/categories";
 import Price from "./Components/Price/Price";
 
+
 export default function PagePLP() {
-  const listCategory = [
-    {category: 'male'},
-    {category: 'female'},
-    {category: 'children'},
-    {size: 'small'},
-    {size: 'medium'},
-    {size: 'large'},
-    {brand: 'lee'},
-    {brand:'levis'},
-    {brand:'wrangler'}
-  ];
+
+    async function getProduct() {
+        const productUrl = "https://58e22bfd-5fc0-4bd8-87d1-44fd691df288.mock.pstmn.io/products";
+        const response = await fetch(productUrl);
+        const data = await response.json();
+        return data;
+    }
+
+    let products=getProduct();
+    console.log(products);
+
+    return (
 
 
-  return (
-
-    <form className="clothes">
+        <form className="clothes">
 
 
-      {listCategory.map(item => {
-        return <Categories category={item.category} size={item.size} brand = {item.brand} class_name='clothes__subclass' />;
-      })}
-      <Price/>
-    </form>
-  );
+            {products.map(item => {
+                return (<Categories category={item.category} fit={item.fit} brand={item.brand}
+                                    class_name='clothes__subclass'/>);
+            })}
+            {/*<Price/>*/}
+        </form>
+    );
 }
-const a = fetch("https://58e22bfd-5fc0-4bd8-87d1-44fd691df288.mock.pstmn.io/products").then(res => console.log(res.json()));
-console.log(a);
+// const a = fetch("https://58e22bfd-5fc0-4bd8-87d1-44fd691df288.mock.pstmn.io/products").then(res => res.json());
+// console.log(a);
