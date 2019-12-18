9c7623c849c4df7bec612e93129a5fdf0f7a8a66
feyziyev007 <azerfeyziyev@hotmail.com>
2019-12-18 14:08:25 +0400

    Header all pages, footer when only need

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Header from './Components/Header/index';
 import PageCart from './Components/PageCart';
 import PageHP from './Components/PageHP';
 import PagePDP from './Components/PagePDP';
@@ -9,6 +10,7 @@ import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 function App() {
   return (
     <Router>
+      <Header />
       <Switch>
         <Route exact path="/" component={PageHP} />
         <Route exact path="/cart" component={PageCart} />
--- src/Components/PageHP/index.js
+++ src/Components/PageHP/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import style from './style.scss';
-import Header from '../Header/index';
 import Slider from './Components/Slider/Slider';
 import ProductList from './Components/ProductList/ProductList';
 import Footer from '../Footer/index';
@@ -9,7 +8,6 @@ export default class PageHP extends React.Component {
   render() {
     return (
       <section class="page-hp">
-        <Header />
         <Slider />
         <ProductList />
         <Footer />
